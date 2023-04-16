a936ad85bc2da08fc9d19d8354d6c6a0b7d83fee
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-04-16 07:36:00 +0500

    Add files via upload

---
+++ Imtihon/js/script.js
@@ -0,0 +1,49 @@
+const navToggle = document.getElementById('navToggle');
+const navbar = document.getElementById('navbarChange');
+const navShrink = document.getElementById('navShrink');
+const li = document.querySelectorAll('.links a')
+const sec = document.querySelectorAll("section")
+
+navToggle.addEventListener("click", () => {
+    navbar.classList.toggle('navOpen')
+    navbar.classList.remove('navbarChange')
+});
+window.addEventListener("scroll", () => {
+    sec.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            li.forEach(link => {
+                link.classList.remove('activeLink');
+                document.querySelector('nav li a[href*='+id+']').classList.add('activeLink')
+            })
+        }
+    })
+})
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset == 0){
+        document.getElementById('gototop').style.display = 'none'
+    } else {
+        document.getElementById('gototop').style.display = 'flex'
+    }
+})
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 80){
+        navShrink.classList.add('shrink')
+    } else {
+        navShrink.classList.remove('shrink')
+    }
+})
+
+function videoPlay() {
+    if(document.querySelector("video").paused){
+        document.querySelector("video").play();
+        document.querySelector('.playBtn').style.display = 'none'
+    } else {
+        document.querySelector("video").pause();
+        document.querySelector('.playBtn').style.display = 'flex'
+    }
+}
